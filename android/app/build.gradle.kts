b917d3102be98e9ba7696b341ba452a567618c86
Thippharake-NaChiengmai <thippharake_na@cmu.ac.th>
2026-02-28 15:27:49 +0700

    set env for build apk and app bundle

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.flutter_adaptive_mobile2"
+    namespace = "com.example.KhanoonApp"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -21,7 +21,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.flutter_adaptive_mobile2"
+        applicationId = "com.example.KhanoonApp"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
--- android/app/src/main/kotlin/com/example/flutter_adaptive_mobile2/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/flutter_adaptive_mobile2/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.flutter_adaptive_mobile2
+package com.example.KhanoonApp
 
 import io.flutter.embedding.android.FlutterActivity
 
